6327e43b5bc6d657fb4e5902895de1a7b4485c9d
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-11-20 15:43:18 +0100

    Add UI of search

--- routes/search.js
+++ routes/search.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/search', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/search', (req, res, next) => {
+  res.render('search', { title: 'Search' });
 });
 
 module.exports = router;
