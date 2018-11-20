fbc08699bc18f1d191e395c7acf9da28d62cbbd8
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-11-20 14:43:55 +0100

    Add link with github api

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request-promise-native')
+const { GITHUB_API_KEY } = require('../api_keys')
 const { setFlash } = require('../utils/flash')
 const router = express.Router();
 
@@ -7,14 +9,31 @@ router.get('/', function (req, res, next) {
   res.redirect(req.session.user ? '/dashboard' : '/signIn')
 });
 
-router.get('/dashboard', function (req, res, next) {
-  if (!req.session.user) {
+router.get('/dashboard', async (req, res, next) => {
+  const user = req.session.user
+
+  if (!user) {
     setFlash(req, 'danger', 'You must be connected to access to the dashboard !')
     res.redirect('/signIn')
     return;
   }
 
-  res.render('dashboard', { title: 'Dashboard', user: req.session.user });
+  const options = {
+    uri: `https://api.github.com/users/${user.nickname}`,
+    qs: { access_token: GITHUB_API_KEY },
+    headers: { 'User-Agent': 'Request-Promise' },
+    json: true // Automatically parses the JSON string in the response
+  };
+
+  let resultUser;
+  let resultUserRepos;
+  try {
+    resultUser = await request(options)
+    options.uri += '/repos'
+    resultUserRepos = await request(options)
+  } catch (_) {}
+
+  res.render('dashboard', { title: 'Dashboard', user: req.session.user, gitUser: resultUser, gitUserRepos: resultUserRepos });
 });
 
 module.exports = router;
